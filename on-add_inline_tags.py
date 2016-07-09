947d218ee322eba5bf22e98ef022191ec4d6550e
David J Patrick <davamundo@gmail.com>
2016-07-09 17:01:35 -0400

    Update on-add_inline_tags.py

--- on-add_inline_tags.py
+++ on-add_inline_tags.py
@@ -12,7 +12,7 @@
 # And on modify, 
 #   a) looks to see if modified task has
 #       :tag1: or :tag2:tag3: in description, and if it does, 
-#   b) looks to see if +tag1 or +tag2 or tag3 exists, if not
+#   b) looks to see if +tag1 or +tag2 or +tag3 exists, if not
 #   c) creates missing tag(s)
 # The script creates tags as needed, but never removes them.
 # Unlike the original script, this version doesn't remove the 
