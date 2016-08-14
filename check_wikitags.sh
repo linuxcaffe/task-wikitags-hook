f67de512497c8a4469ec277ba2909cc10817d36e
David J Patrick <davamundo@gmail.com>
2016-08-14 18:28:31 -0400

    added check_wikitags.sh stubb

---
+++ check_wikitags.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#
+# check_wikitags.sh
+# Copyright (C) 2016 djp <djp@cutter>
+#
+# Distributed under terms of the MIT license.
+#
+# this script designed to check for wikitags, in any task description, that 
+# do not have corresponding taskwarrior tags.
+
+
