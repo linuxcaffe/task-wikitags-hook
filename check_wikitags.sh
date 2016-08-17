140e826f70078ce57d4180ee9196a7e1786c15ce
David J Patrick <davamundo@gmail.com>
2016-08-17 00:07:32 -0400

    added wikitags regex & count

--- check_wikitags.sh
+++ check_wikitags.sh
@@ -8,4 +8,9 @@
 # this script designed to check for wikitags, in any task description, that 
 # do not have corresponding taskwarrior tags.
 
+# task executable
+# taskrc
+# taskdata
 
+echo -e "You have `task +PENDING '/(^| ):([^:]+:)+( |$)/' count` pending tasks with wikitags"
+exit
